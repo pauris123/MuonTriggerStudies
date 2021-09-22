d6a0ca3b37bae6c0bc06537515c82d9e433591e2
Normunds Strautnieks <79871354+pauris123@users.noreply.github.com>
2021-09-22 14:38:26 +0300

    Update Muon_dR_Tasks.py

--- Muon_dR_Tasks.py
+++ Muon_dR_Tasks.py
@@ -46,29 +46,50 @@ for aiziet in range(notik,tik):
     events = Events("root://eoscms/"+rootfile)
     #print(" {0}\tI have the tree".format(l_wall_time(time.time()-start)))
     
-    muon_charge = [] # Lists for read in L1T muons
-    muon_pt = []
-    muon_eta = []
-    muon_phi = []
-    muon_iev = []
-    muon_i = []
-    
-    muon_pdgId = [] # Lists for decayed muons from Chi->J/Psi->MuMu in GenP  (1st task)!
-    muon_pt_gen = []
-    muon_eta_gen = []
-    muon_phi_gen = []
-    muon_iev_gen = []
-    muon_i_gen = []
     
     di_muon_dR_genP = [] # dR between muons that come from Chi->J/Psi-> Mu+Mu decay in GenParticles part (2nd task)!
     di_muon_dR_genP_iev = [] # List to help identify the Chi (event) that this dR is calculated for
     di_muon_dR_genP_i = [] # List to help identify the Chi (i) that this dR is calculated for
     
+    muon_dR_min = [] # Matching genP and L1T muons by dR, the list of minimal/matching dR 3rd task.
+    muon_dR_min_iev = []
+    muon_dR_min_genP_i = []
+    muon_dR_min_L1T_i = []
     
+    di_muon_inv_mass = []        # Lists for dR, inv.M and in.M/dR for matched L1T di-muons reconstructed from Chi decay - The end result? 4th task
+    di_muon_dR_matched_L1T = []
+    inv_M_o_dR_di_muon = []
+            
     for iev,event in enumerate(events): # Reading in events from the file
         event.getByLabel(l1MuonLabel, l1Muons)
         event.getByLabel(genParticlesLabel, genParticles)
 
+        muon_charge = [] # Lists for read in L1T muons
+        muon_pt = []
+        muon_eta = []
+        muon_phi = []
+        muon_iev = []
+        muon_i = []
+    
+        muon_pdgId = [] # Lists for decayed muons from Chi->J/Psi->MuMu in GenP  (1st task)!
+        muon_pt_gen = []
+        muon_eta_gen = []
+        muon_phi_gen = []
+        muon_iev_gen = []
+        muon_i_gen = []
+        
+        muon_dR_min_matched = []  # Same as muon_dR_min, but for each event, makes writing data more simpler in the next if and for cycles
+        muon_matched_L1T_number = []
+                # Matched L1T muon parameters to genP muons. 1st L1T matched muon in this list corresponds to the first muon in genP list 
+    # So the list might go - (10th L1T muon -> 1st GenP muon), (24th L1T muon -> 2nd GenP muon) and so on, so have to follow the iev and i numbers of L1T muon
+    
+        muon_matched_L1T_charge = []  # Lists of matched L1T muon parameters, so it is easier to calculate stuff onwards
+        muon_matched_L1T_pt = []
+        muon_matched_L1T_eta = []
+        muon_matched_L1T_phi = []
+        muon_matched_L1T_iev = []
+        muon_matched_L1T_i = []
+        
         # l1Muons
         for i,l1Mu in enumerate(l1Muons.product()): # Checking all L1T muons in single event
             
@@ -78,7 +99,7 @@ for aiziet in range(notik,tik):
             muon_phi.append(l1Mu.phi()) 
             muon_iev.append(iev)
             muon_i.append(i)   
-    
+            
          #genParticles      
         for i,genP in enumerate(genParticles.product()):
             if (genP.pdgId() == 20443 and genP.numberOfDaughters() == 2) : # Takes Chi that has 2 daughters
@@ -137,46 +158,81 @@ for aiziet in range(notik,tik):
         
                 
     
-    muon_dR_min = [] # Matching genP and L1T muons by dR, the list of minimal/matching dR
-    muon_matched_L1T_number = [] # Matched L1T muon parameters to genP muons. 1st L1T matched muon in this list corresponds to the first muon in genP list 
-    # So the list might go - (10th L1T muon -> 1st GenP muon), (24th L1T muon -> 2nd GenP muon) and so on, so have to follow the iev and i numbers of L1T muon
-    
-    muon_matched_L1T_charge = []  # Lists of matched L1T muon parameters, so it is easier to calculate stuff onwards
-    muon_matched_L1T_pt = []
-    muon_matched_L1T_eta = []
-    muon_matched_L1T_phi = []
-    muon_matched_L1T_iev = []
-    muon_matched_L1T_i = []
-    
-    
-    #Calculating the dR values for each genP muon matched with every L1T muon
-    for j in range(len(muon_pdgId)): # Take the genP muon
-    
-        muon_dR_min_calc = []
-    
-        for k in range(len(muon_charge)): # Check the the chosen genP muon with all the usable L1T muons
-            if (muon_charge[k] == -1 and muon_pdgId[j] == 13) or (muon_charge[k] == 1 and muon_pdgId[j] == -13):  
-                
-                muon_dR_min_calc.append(((muon_eta[k]-muon_eta_gen[j])**2+(muon_phi[k]-muon_phi_gen[j])**2)**0.5) #Calculate dR, add it to the list
-        
-            else:
-                continue
-        
-        muon_dR_min.append(min(muon_dR_min_calc))  # Take the min value of dR and add it to the list for matched muon dR's
-        muon_matched_L1T_number.append(muon_dR_min_calc.index(min(muon_dR_min_calc)))  # Find the index of the L1T muon, that gave the lowest dR value for the particular genP muon, and add it to the extra list.
-    
-    for n in range(len(muon_matched_L1T_number)): # Creates new L1T muon parameter lists, based of matching. So first element in these list is matched L1T muon paramaters, that got matched to the 1st muon in genP list. 2nd elemnt here is matched L1T muon for the 2nd genP. Every two lines are a pair. 1st and 2nd is from the 1st Chi matched, 3rd and 4th is from the 2nd Chi matched and so on.
-        
-        muon_matched_L1T_charge.append(muon_charge[muon_matched_L1T_number[n]])    
-        muon_matched_L1T_pt.append(muon_pt[muon_matched_L1T_number[n]])
-        muon_matched_L1T_eta.append(muon_eta[muon_matched_L1T_number[n]])
-        muon_matched_L1T_phi.append(muon_phi[muon_matched_L1T_number[n]])
-        muon_matched_L1T_iev.append(muon_iev[muon_matched_L1T_number[n]])
-        muon_matched_L1T_i.append(muon_i[muon_matched_L1T_number[n]])
-    
-    if len(muon_pdgId) == len(muon_matched_L1T_charge): # Checking whether all genP muons got matched with something
-        print("All gucci with the matching")
 
+            
+    
+        #Calculating the dR values for each genP muon matched with every L1T muon
+        if (len(muon_pdgId) == 2 or len(muon_pdgId) == 4) and len(muon_charge) >= 2 :
+            if len(muon_charge) == 2 and sum(muon_charge) != 0:
+                continue
+            if len(muon_charge) == 3 and abs(sum(muon_charge)) == 3:
+                continue
+            if len(muon_charge) == 4 and abs(sum(muon_charge)) == 4:
+                continue
+            if len(muon_charge) == 5 and abs(sum(muon_charge)) == 5:
+                continue
+            if len(muon_charge) == 6 and abs(sum(muon_charge)) == 6:
+                continue
+                        
+            for j in range(len(muon_pdgId)): # Take the genP muon
+    
+                muon_dR_min_calc = []
+    
+                for k in range(len(muon_charge)): # Check the the chosen genP muon with all the usable L1T muons
+                    if (muon_charge[k] == -1 and muon_pdgId[j] == 13) or (muon_charge[k] == 1 and muon_pdgId[j] == -13):  
+                
+                        muon_dR_min_calc.append(((muon_eta[k]-muon_eta_gen[j])**2+(muon_phi[k]-muon_phi_gen[j])**2)**0.5) #Calculate dR, add it to the list
+        
+                    else:
+                        continue
+        
+                muon_dR_min.append(min(muon_dR_min_calc))  # Take the min value of dR and add it to the list for matched muon dR's
+                muon_dR_min_matched.append(min(muon_dR_min_calc))
+                muon_matched_L1T_number.append(muon_dR_min_calc.index(min(muon_dR_min_calc)))  
+                # Find the index of the L1T muon, that gave the lowest dR value for the particular genP muon, and add it to the extra list.
+                muon_dR_min_iev.append(iev)
+                muon_dR_min_genP_i.append([muon_i_gen[j]])
+            
+            for q in range(len(muon_matched_L1T_number)):
+                print("Checking for repeated matching of the same muon, same number below, double matching... Need a fix")
+                print(muon_matched_L1T_number[q])
+                
+    
+            for n in range(len(muon_matched_L1T_number)): 
+    # Creates new L1T muon parameter lists, based of matching. So first element in these list is matched L1T muon paramaters, that got matched to the 1st muon in genP list. 2nd elemnt here is matched L1T muon for the 2nd genP. Every two lines are a pair. 1st and 2nd is from the 1st Chi matched, 3rd and 4th is from the 2nd Chi matched and so on.
+        
+                muon_matched_L1T_charge.append(muon_charge[muon_matched_L1T_number[n]])    
+                muon_matched_L1T_pt.append(muon_pt[muon_matched_L1T_number[n]])
+                muon_matched_L1T_eta.append(muon_eta[muon_matched_L1T_number[n]])
+                muon_matched_L1T_phi.append(muon_phi[muon_matched_L1T_number[n]])
+                muon_matched_L1T_iev.append(muon_iev[muon_matched_L1T_number[n]])
+                muon_matched_L1T_i.append(muon_i[muon_matched_L1T_number[n]])
+                
+                muon_dR_min_L1T_i.append(muon_i[muon_matched_L1T_number[n]])
+                
+            if len(muon_pdgId) == len(muon_matched_L1T_charge): # Checking whether all genP muons got matched with something
+                print("All gucci with the matching count")
+                
+                if (muon_matched_L1T_charge[0]+muon_matched_L1T_charge[1] != 0):
+                    print("Something is wrong with the matched muon charges or the numbering in the list")
+                
+                if len(muon_matched_L1T_charge) == 2:
+                
+                    muon1 = ROOT.TLorentzVector()
+                    muon2 = ROOT.TLorentzVector()
+
+                    muon1.SetPtEtaPhiM(muon_matched_L1T_pt[0],
+                                     muon_matched_L1T_eta[0],
+                                     muon_matched_L1T_phi[0],0.10566)
+                    muon2.SetPtEtaPhiM(muon_matched_L1T_pt[1],
+                                     muon_matched_L1T_eta[1],
+                                     muon_matched_L1T_phi[1],0.10566)
+            
+                    di_muon_inv_mass.append((muon1+muon2).M())
+                    di_muon_dR_matched_L1T.append(((muon_matched_L1T_eta[0]-muon_matched_L1T_eta[1])**2+(muon_matched_L1T_phi[0]-muon_matched_L1T_phi[1])**2)**0.5)
+                    inv_M_o_dR_di_muon.append(((muon1+muon2).M()/((muon_matched_L1T_eta[0]-muon_matched_L1T_eta[1])**2+(muon_matched_L1T_phi[0]-muon_matched_L1T_phi[1])**2)**0.5))
+                else:
+                    print("We have more than 2 matched L1T muons, probably 4, need to upgrade the code for dR and inv.M/dR calculation")
     
     # Here Im writing info to files, but havent started that.
             
@@ -190,10 +246,20 @@ for aiziet in range(notik,tik):
      #   fom.write(str(muon_dR_min[v])+"\t"+str(aiziet)+"\n")
     #fom.close()
     
-    ##fon=open("Muon_dR_from_JPsi_genP.txt","a")
-    ##for b in range(len(di_muon_dR_genP)):
-    ##    fon.write(str(di_muon_dR_genP[b])+"\t"+str(di_muon_dR_genP_iev[b])+"\t"+str(di_muon_dR_genP_i[b])+"\n")
-    ##fon.close()                
+    fon=open("Muon_dR_from_JPsi_genP.txt","a")
+    for b in range(len(di_muon_dR_genP)):
+        fon.write(str(di_muon_dR_genP[b])+"\t"+str(di_muon_dR_genP_iev[b])+"\t"+str(di_muon_dR_genP_i[b])+"\t"+str(aiziet)+"\n")
+    fon.close()
+    
+    fob=open("Muon_dR_from_Matching_genP_L1T.txt","a")
+    for h in range(len(muon_dR_min)):
+        fob.write(str(muon_dR_min[h])+"\t"+str(muon_dR_min_iev[h])+"\t"+str(muon_dR_min_genP_i[h])+"\t"+str(muon_dR_min_L1T_i[h])+"\t"+str(aiziet)+"\n")
+    fob.close()
+    
+    fov=open("Di_Muon_dR_invM_invM_o_dR.txt","a")
+    for d in range(len(di_muon_dR_matched_L1T)):
+        fob.write(str(di_muon_dR_matched_L1T[d])+"\t"+str(di_muon_inv_mass[d])+"\t"+str(inv_M_o_dR_di_muon[d])+"\t"+str(aiziet)+"\n")
+    fov.close()                
                 
     #print("List of cross-checked muon dR is this long -> "+str(len(muon_dR)))
     #print("List of cross-checked muon best min dR is this long -> "+str(len(muon_dR_min)))             
@@ -201,4 +267,4 @@ for aiziet in range(notik,tik):
                 
 print("{0}\tAll done".format(wall_time(time.time()-bigBang)))
                               
-                               
+                                                         
